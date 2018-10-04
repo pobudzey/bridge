67daf80133cd019cc9bc3ab695bfa44e10305775
Maxim Pobudzey <maxim.pobudzey@net4user.com>
2018-10-04 17:39:24 -0400

    Removed unwanted backslash causing a logical error #16

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
 	SECRET_KEY = 'soen341flaskapp'
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
