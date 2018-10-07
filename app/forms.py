3d266954f41a10d7e3f1f20dcf6d69845cb0764f
Jesse Serre <JesseSerre@hotmail.com>
2018-10-07 19:55:38 -0400

    Created signup.html and made necessary modifications to forms.py, routes.py and base.html

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, validators
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 	remember_me = BooleanField('Remember Me')
-	submit = SubmitField('Log in') 
+	submit = SubmitField('Log in')
+
+class SignupForm(FlaskForm):
+    firstname = StringField('First Name', [validators.Length(min=3, max=20)])
+    lastname = StringField('Last Name', [validators.Length(min=3, max=20)])
+    username = StringField('Username', [validators.Length(min=6, max=20)])
+    email = StringField('Email Address', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Password must match')
+        ])
+    confirm = PasswordField('Repeat Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+    submit = SubmitField('Sign up')
+    
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm
+from app.forms import LoginForm, SignupForm
 
 @app.route('/')
 @app.route('/index')
@@ -14,3 +14,8 @@ def login():
 		flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
 		return redirect(url_for('index'))
 	return render_template('login.html', title='Log in', form=form)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+	form = SignupForm()
+	return render_template('signup.html', title='Sign Up', form=form)
