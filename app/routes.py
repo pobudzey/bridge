ce9d37e4db6603d74d6a32e9ba9319dc04bfadba
Maxim Pobudzey <maxim.pobudzey@net4user.com>
2018-10-15 17:14:44 -0400

    Added 'request' import from flask #31

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,8 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from app import app
 from app.forms import LoginForm
-from flask_login import current_user, login_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
-from flask_login import logout_user, login_required
 from werkzeug.urls import url_parse
 
 #Index view function
