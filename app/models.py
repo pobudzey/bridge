ae7d7830941ede1d73b1eb29f75775e6a666967d
Badreddine Loulidi <badred123456@gmail.com>
2018-10-19 12:30:12 -0400

    Added about attribute to the User model #44

--- app/models.py
+++ app/models.py
@@ -10,7 +10,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
-
+    about = db.Column(db.String(140))
+    
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
