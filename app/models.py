0ce35a14285c76bd86454a5e87a17fa007b1a917
Maxim Pobudzey <maxim.pobudzey@net4user.com>
2018-10-28 18:11:42 -0400

    Added a Message database model #50

--- app/models.py
+++ app/models.py
@@ -34,3 +34,14 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+#Message database model
+class Message(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+	body = db.Column(db.String(500))
+	timestamp = db.Column(db.DateTime, index = True, default = datetime.utcnow)
+	
+	def __repr__(self):
+		return '<Message {}>'.format(self.body)
