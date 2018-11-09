761c8a12545af62dddb521916b17c78635aaa7d7
Marc Tohme <marctohme@hotmail.com>
2018-11-09 12:23:00 -0500

    Added unit tests for login, logout and signup page functionality

--- tests.py
+++ tests.py
@@ -6,8 +6,10 @@ class UserModelCase(unittest.TestCase):
 
 	#Creates an in-memory database for testing purposes	
 	def setUp(self):
-		app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
 		db.create_all()
+		app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+		self.app = app.test_client()
+		self.app.testing = True 
 	
 	#Deletes the in-memory database
 	def tearDown(self):
@@ -20,6 +22,21 @@ class UserModelCase(unittest.TestCase):
 		u.set_password('goodpassword')
 		self.assertFalse(u.check_password('wrongpassword'))
 		self.assertTrue(u.check_password('goodpassword'))
+
+	#Login functionality test
+	def test_login(self):
+		response = self.app.post('/login', data=dict(
+			    username='Marct', password='12qwaszx'), follow_redirects=True)
+
+	#Logout functionality test	
+	def test_logout(self):
+	    response = self.app.get('/logout', follow_redirects=True)
+
+	#Signup page test
+	def test_signup(self):
+		response = self.app.post('/signup', data = dict(
+				username='Marc', password='gertrecw', password2='gertrecw', 
+				email='email@email.com'), follow_redirects=True)
 
 if __name__ == '__main__':
 	unittest.main()
