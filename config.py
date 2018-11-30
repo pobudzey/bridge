4d26ddabf845a7be5d394e3609487eaf6c84abd6
Maxim Pobudzey <maxim.pobudzey@net4user.com>
2018-11-29 20:56:34 -0500

    Added Flask-Uploads configuration variables #82

--- config.py
+++ config.py
@@ -3,6 +3,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 #Configuration options for Flask
 class Config(object):
-	SECRET_KEY = 'soen341flaskapp'
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-	SQLALCHEMY_TRACK_MODIFICATIONS = False
+  SECRET_KEY = 'soen341flaskapp'
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
+  UPLOADED_IMAGES_DEST = basedir + '/app/static/img/'
+  UPLOADED_IMAGES_URL = 'http://localhost:5000/static/img/'
